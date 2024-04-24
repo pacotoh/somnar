fa222e78be7c01ebc621b65bbee92ece378e2b00
pacotoh <pacgallego@gmail.com>
2024-04-25 00:08:37 +0200

    data_cleaning to clean sleep table data

--- src/cleaning/data_cleaning.py
+++ src/cleaning/data_cleaning.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+
+def clean_naps() -> None:
+    pass
+
+
+def drop_sleep_unused_fields(df: pd.DataFrame, path: str) -> None:
+    return df.drop(['naps', 'REMTime'], axis=1).to_csv(path)
+
+
+if __name__ == '__main__':
+    drop_sleep_unused_fields(df=pd.read_csv('../../data/sleep.csv'),
+                             path='../../data/sleep.csv')
