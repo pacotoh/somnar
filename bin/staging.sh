cef7578df762150057cee2cf8dc2f6df2a5448b2
pacotoh <pacgallego@gmail.com>
2024-04-25 09:54:45 +0200

    Initialization script first version tested

--- bin/staging.sh
+++ bin/staging.sh
@@ -8,12 +8,16 @@ find "$source" -type f -name '*.csv' -exec mv {} "$dest" \;
 
 for file in "$dest"/*; do
   if [ -f "$file" ]; then
-    echo "$file"
     target=${file%_*}.csv
-    echo "$target"
     lowercase_target=$(echo "$target" | tr '[:upper:]' '[:lower:]')
     mv -- "$file" "$lowercase_target"
   fi
 done
 
 rm -rf "$source"
+find "../data" -type f ! -name "*activity.csv" ! -name "*activity_minute.csv" ! -name "*activity_stage.csv" ! -name "*heartrate_auto.csv" ! -name "*sleep.csv" ! -name "*init.sql" ! -name "*sport.csv" -delete
+cp '../db/init.sql' '../data/'
+
+python '../src/cleaning/data_cleaning.py' '../data/sleep.csv'
+
+echo "Data Initialization Process has completed."
--- src/cleaning/data_cleaning.py
+++ src/cleaning/data_cleaning.py
@@ -1,14 +1,25 @@
 import pandas as pd
+import re
+import sys
 
 
-def clean_naps() -> None:
-    pass
+def clean_naps(file_path: str) -> None:
+    with open(file_path, 'r') as sleep:
+        text = sleep.read()
+        cl_text = re.sub(pattern='\".*', repl='', string=text)
+    with open(file_path, 'w') as sleep:
+        sleep.write(cl_text)
 
 
 def drop_sleep_unused_fields(df: pd.DataFrame, path: str) -> None:
-    return df.drop(['naps', 'REMTime'], axis=1).to_csv(path)
+    return df.drop(['naps', 'REMTime'], axis=1).to_csv(path, index=False)
 
 
 if __name__ == '__main__':
-    drop_sleep_unused_fields(df=pd.read_csv('../../data/sleep.csv'),
-                             path='../../data/sleep.csv')
+    if len(sys.argv) > 1:
+        sleep_path = sys.argv[1]
+        clean_naps(file_path=sleep_path)
+        df_sleep = pd.read_csv(sleep_path)
+        drop_sleep_unused_fields(df_sleep, sleep_path)
+    else:
+        print("ERROR: No arguments provided")
