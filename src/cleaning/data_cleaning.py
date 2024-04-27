5fb552432835687793b1be1deaacf6202a6aa519
pacotoh <pacgallego@gmail.com>
2024-04-27 12:45:23 +0200

    sleep header method added

--- src/cleaning/data_cleaning.py
+++ src/cleaning/data_cleaning.py
@@ -3,6 +3,8 @@ import re
 
 SLEEP_PATH = '../data/sleep.csv'
 SPORT_PATH = '../data/sport.csv'
+HR_PATH = '../data/heartrate_auto.csv'
+ACTIVITY_PATH = '../data/activity.csv'
 
 
 def clean_sleep_naps() -> None:
@@ -16,6 +18,17 @@ def clean_sleep_naps() -> None:
 def drop_sleep_unused_fields() -> None:
     sleep_df = pd.read_csv(SLEEP_PATH)
     sleep_df.drop(['naps', 'REMTime'], axis=1).to_csv(SLEEP_PATH, index=False)
+
+
+def modify_sleep_header() -> None:
+    sleep_df = pd.read_csv(SLEEP_PATH)
+    sleep_df.rename(columns={
+        'deepSleepTime': 'deep_sleep_time',
+        'shallowSleepTime': 'shallow_sleep_time',
+        'wakeTime': 'wake_time'
+    }, inplace=True)
+
+    sleep_df.to_csv(SLEEP_PATH, index=False)
 
 
 def modify_sport_header() -> None:
@@ -33,7 +46,33 @@ def modify_sport_header() -> None:
     sport_df.to_csv(SPORT_PATH, index=False)
 
 
-if __name__ == '__main__':
+def modify_heartrate_header() -> None:
+    hr_df = pd.read_csv(HR_PATH)
+    hr_df.rename(columns={
+        'heartRate': 'heart_rate'
+    }, inplace=True)
+
+    hr_df.to_csv(HR_PATH, index=False)
+
+
+def modify_activity_header() -> None:
+    act_df = pd.read_csv(ACTIVITY_PATH)
+    act_df.rename(columns={
+        'runDistance': 'run_distance'
+    }, inplace=True)
+
+    act_df.to_csv(ACTIVITY_PATH, index=False)
+
+
+def clean_csv_data() -> None:
     clean_sleep_naps()
     drop_sleep_unused_fields()
+
+    modify_sleep_header()
     modify_sport_header()
+    modify_activity_header()
+    modify_heartrate_header()
+
+
+if __name__ == '__main__':
+    clean_csv_data()
