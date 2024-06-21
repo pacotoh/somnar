19588befc923bdafb3440105cd002847ab8db18c
pacotoh <pacgallego@gmail.com>
2024-06-21 12:33:12 +0200

    Need to FIX errors in Holt Winters implementation

--- src/holt_winters/holt_winters.py
+++ src/holt_winters/holt_winters.py
@@ -1,22 +1,29 @@
 import pandas as pd
-from statsmodels.tsa.api import ExponentialSmoothing
+import statsmodels.tsa.api as tsa
 from sklearn.metrics import root_mean_squared_error
 
 
-def holt_winters_grid(df: pd.DataFrame):
+def split_train_test(df: pd.DataFrame, field: str) -> list[pd.DataFrame]:
+    # TODO: Split by year or semester -> 5 months or 11 months to predict 1
+    pass
+
+
+# FIXME: FIX fit creation and root_mean_squared_error error
+def holt_winters_grid(df: pd.DataFrame, field: str) -> list:
     holt_grid = []
     opt = ['add', 'mul']
 
     for i in range(2, 13):
         for t in opt:
             for s in opt:
-                fit = ExponentialSmoothing(df[0] ,seasonal_periods=i, trend=t, seasonal=s).fit()
-                fc = fit.forecast(len(df[0]))
-                rmse = root_mean_squared_error(df[0], fc)
+                fit = tsa.ExponentialSmoothing(df[field], seasonal_periods=i, trend=t, seasonal=s).fit()
+                fc = fit.forecast(len(df[field]))
+                rmse = root_mean_squared_error(df[field], fc)
                 holt_grid.append([[i, t, s], fit.bic, fit.aic, rmse])
 
     return holt_grid
 
 
 if __name__ == '__main__':
-    pass
+    df_sleep = pd.read_csv('../../data/sleep.csv')
+    holt_winters_grid(df_sleep, 'deep_sleep_time')
